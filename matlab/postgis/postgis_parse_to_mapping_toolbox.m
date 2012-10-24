33be33d485750fa8d30d6132b62ce18763759604
Ádám Barancsuk <adam.barancsuk@gmail.com>
2012-10-24 11:41:26 +0200

    Separated PostGIS-related MATLAB code.

--- matlab/postgis/postgis_parse_to_mapping_toolbox.m
+++ matlab/postgis/postgis_parse_to_mapping_toolbox.m
@@ -11,5 +11,7 @@ function [s] = postgis_parse_to_mapping_toolbox(conn, query, varargin)
     lon = cell2mat(data(:,1));
     lat = cell2mat(data(:,2));
     
+    fprintf ('%d points\n', sum(~isnan(lon)));
+    
     s = geostruct_from_latlon(lat, lon, type);
 end
